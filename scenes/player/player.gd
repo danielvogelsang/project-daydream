98498e3e8906e23293865ee0f783cbc09d940008
danielvogelsang <daniel@vogelsang-lr.de>
2025-02-09 22:58:14 +0100

    added coyote time and air resistance bugfix

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,10 +6,14 @@ const GRAVITY: float = 800.0
 const MAX_FALL_SPEED: float = 400
 
 @onready var anim_player: AnimatedSprite2D = $AnimatedSprite2D
+@onready var debug_label: Label = $DebugLabel
+@onready var coyote_timer: Timer = $CoyoteTimer
 
 var air_resistance: float = 100
-var air_speed: float
-var in_air: bool
+var air_speed: float = SPEED
+var in_air: bool = false
+var jump_available: bool = true
+var coyote_time: float = 0.3
 
 enum PlayerState {
 	IDLE,
@@ -23,6 +27,7 @@ var current_state: PlayerState = PlayerState.IDLE
 func calculate_states() -> void:
 	if is_on_floor():
 		in_air = false
+		jump_available = true
 		if velocity.x == 0:
 			change_state(PlayerState.IDLE)
 		else:
@@ -51,8 +56,12 @@ func change_state(new_state: PlayerState):
 
 func get_input() -> void:
 	velocity.x = 0
+	
 	if in_air: 
 		air_speed = SPEED - air_resistance
+	else: 
+		air_speed = SPEED
+		
 	if Input.is_action_pressed("left"):
 		velocity.x = - air_speed
 		anim_player.flip_h = true
@@ -60,18 +69,33 @@ func get_input() -> void:
 		velocity.x = air_speed
 		anim_player.flip_h = false
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_available:
 		velocity.y = JUMP_VELOCITY
-		
-	# keeps falling speed between JUMP_VELOCITY, MAX_FALL_SPEED
+		coyote_timeout()
+	
+	# keeps falling speed between JUMP_VELOCITY and MAX_FALL_SPEED
 	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL_SPEED)
 
-func check_if_falling(delta: float) -> void:
+func handle_falling(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
-		
+
+func handle_coyote_timer() -> void:
+	if jump_available:
+		if coyote_timer.is_stopped():
+			coyote_timer.start(coyote_time)
+
+# set to false after timeout
+func coyote_timeout() -> void:
+	jump_available = false
+
+func update_debug_label() -> void:
+	debug_label.text = "jump available: %s" % [jump_available]
+
 func _physics_process(delta: float) -> void:
-	check_if_falling(delta)
+	handle_falling(delta)
 	get_input()
 	move_and_slide()
 	calculate_states()
+	handle_coyote_timer()
+	update_debug_label()
