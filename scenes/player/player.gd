8b6a4a294be9fd0467aebdc90d4e74c67c0147fb
danielvogelsang <daniel@vogelsang-lr.de>
2025-02-10 15:55:16 +0100

    code cleanup and todo added

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -27,14 +27,11 @@ var current_state: PlayerState = PlayerState.IDLE
 
 func calculate_states() -> void:
 	if is_on_floor():
-		in_air = false
-		jump_available = true
 		if velocity.x == 0:
 			change_state(PlayerState.IDLE)
 		else:
 			change_state(PlayerState.RUNNING)
 	else:
-		in_air = true
 		if velocity.y > 0:
 			change_state(PlayerState.FALLING)
 		else:
@@ -56,13 +53,8 @@ func change_state(new_state: PlayerState):
 			anim_player.play("Falling")
 
 func get_input() -> void:
-	velocity.x = 0
+	handle_air_speed()
 	
-	if in_air: 
-		air_speed = SPEED - air_resistance
-	else: 
-		air_speed = SPEED
-		
 	if Input.is_action_pressed("left"):
 		velocity.x = - air_speed
 		anim_player.flip_h = true
@@ -71,19 +63,33 @@ func get_input() -> void:
 		anim_player.flip_h = false
 	
 	# small jumps
+	# TODO: fix flying by spamming jump
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y = DIMINISHING_JUMP_VELOCITY
 	
 	if Input.is_action_just_pressed("jump") and jump_available:
 		velocity.y = JUMP_VELOCITY
 		coyote_timeout()
-	
-	# keeps falling speed between JUMP_VELOCITY and MAX_FALL_SPEED
-	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL_SPEED)
+
+func is_in_air() -> void:
+	if is_on_floor():
+		in_air = false
+		jump_available = true
+	else: in_air = false
+
+func handle_air_speed() -> void:
+	velocity.x = 0
+	if in_air: 
+		air_speed = SPEED - air_resistance
+	else: 
+		air_speed = SPEED
 
 func handle_falling(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
+		
+	# keeps falling speed between JUMP_VELOCITY and MAX_FALL_SPEED
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL_SPEED)
 
 func handle_coyote_timer() -> void:
 	if jump_available:
@@ -102,5 +108,6 @@ func _physics_process(delta: float) -> void:
 	get_input()
 	move_and_slide()
 	calculate_states()
+	is_in_air()
 	handle_coyote_timer()
 	update_debug_label()
